aaca1a4ff18fe02de7739303e104af4b10294612
admin <ye6585055@gmail.com>
2025-05-12 18:52:32 +0900

    10주차 수업진도까지

--- js/js_popup.js
+++ js/js_popup.js
@@ -1,9 +1,21 @@
+/*
 function popup() {
   window.open(
     "../popup/popup.html",
     "팝업테스트",
     "width=400, height=300, top=10, left=10"
   );
+}
+*/
+function pop_up() {
+  var cookieCheck = getCookie("popupYN");
+  if (cookieCheck != "N") {
+    window.open(
+      "../popup/popup.html",
+      "팝업테스트",
+      "width=400, height=300, top=10, left=10"
+    );
+  }
 }
 
 /* 
@@ -40,9 +52,44 @@ function show_clock() {
   setTimeout(show_clock, 1000); //1초마다 갱신
 }
 
+/*
 function over(obj) {
   obj.src = "../Images/RG_RED.png";
 }
+  */
+const over = (obj) => {
+  obj.src = "../Images/RG_RED.png";
+};
+const search_message = () => {
+  const c = "검색을 수행합니다";
+  alert(c);
+};
 function out(obj) {
   obj.src = "../Images/RG_WHITE.png";
+}
+
+function setCookie(name, value, expiredays) {
+  var date = new Date();
+  date.setDate(date.getDate() + expiredays);
+  document.cookie =
+    escape(name) +
+    "=" +
+    escape(value) +
+    "; expires=" +
+    date.toUTCString() +
+    "; path=/";
+}
+function getCookie(name) {
+  var cookie = document.cookie;
+  console.log("쿠키를 요청합니다.");
+  if (cookie != "") {
+    var cookie_array = cookie.split("; ");
+    for (var index in cookie_array) {
+      var cookie_name = cookie_array[index].split("=");
+      if (cookie_name[0] == "popupYN") {
+        return cookie_name[1];
+      }
+    }
+  }
+  return;
 }
